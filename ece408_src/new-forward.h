6df4ad26b6846389dcbfd7d20f5ff194ffc73db6
ghe10 <gche1993@gmail.com>
2017-11-13 19:54:13 -0600

    Milestone 2 done

--- ece408_src/new-forward.h
+++ ece408_src/new-forward.h
@@ -12,7 +12,8 @@ namespace op
 // This function is called by new-inl.h
 // Any code you write should be executed by this function
 template <typename cpu, typename DType>
-void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DType> &x, const mshadow::Tensor<cpu, 4, DType> &k)
+void forward(mshadow::Tensor<cpu, 4, DType> &y,
+  const mshadow::Tensor<cpu, 4, DType> &x, const mshadow::Tensor<cpu, 4, DType> &k)
 {
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -21,28 +22,41 @@ void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DT
     The goal here is to be correct, not fast (this is the CPU implementation.)
     */
 
-    
-    //const int B = x.shape_[0];
-    // const int M = y.shape_[1];
-    // const int C = x.shape_[1];
-    // const int H = x.shape_[2];
-    // const int W = x.shape_[3];
-    // const int K = w.shape_[3];
-
-   // for (int b = 0; b < B; ++b) {
-     //   CHECK_EQ(0, 1) << "Missing an ECE408 CPU implementation!";
-
-        /* ... a bunch of nested loops later...
-            y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
-        */
-    //}
+    // k : [output feature map][input feature map][y][x]
     const int batch_size = x.shape_[0];
-    const int input_channels = x.shape_[1];
-    const int input_y = x.shape_[2];
-    const int input_x = x.shape_[3];
-    const int output_channles = k.shape_[0];
+    const int input_channels = x.shape_[1]; //C
+    const int input_y_size = x.shape_[2]; // h
+    const int input_x_size = x.shape_[3]; // w
+    const int output_channels = k.shape_[0];
+    const int kernel_y = k.shape_[2];
+    const int kernel_x = k.shape_[3];
+    const int output_y_size = y.shape_[2];
+    const int output_x_size = y.shape_[3];
 
-    
+    for (int batch_index = 0; batch_index < batch_size; batch_index++) {
+      for (int output_channel = 0; output_channel < output_channels; output_channel++) {
+        // next we loop through all the output elements in a output channel
+        for (int out_y = 0; out_y < output_y_size; out_y++) {
+          for (int out_x = 0; out_x < output_x_size; out_x++) {
+            y[batch_index][output_channel][out_y][out_x] = 0;
+            // next we go through all the pixels related to y at this position
+            for (int input_channel = 0; input_channel < input_channels; input_channel++) {
+              for (int dy = 0; dy < kernel_y; dy++) {
+                for (int dx = 0; dx < kernel_x; dx++) {
+                  int yy = out_y + dy;
+                  int xx = out_x + dx;
+                  //if (yy < input_y_size && xx < input_x_size) {
+                  y[batch_index][output_channel][out_y][out_x] =
+                    y[batch_index][output_channel][out_y][out_x] +
+                    x[batch_index][input_channel][yy][xx] *
+                    k[output_channel][input_channel][dy][dx];
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 }
 }
 }
