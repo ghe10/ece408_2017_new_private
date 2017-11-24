4c9915db2f93172d166848e8c4d259c350b5e392
ghe10 <gche1993@gmail.com>
2017-11-24 00:33:58 -0600

    Fast conv....

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -3,87 +3,135 @@
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
 
 #include <mxnet/base.h>
-
-#define TILE_WIDTH 12
+#define TILE_WIDTH 25
+#define TILE_HEIGHT 25
 namespace mxnet
 {
 namespace op
 {
 
+  template<typename gpu, typename DType>
+  __global__ void forward_kernel(DType *y, const DType *B, const DType *A,  const int C, const int H, const int W, const int K) {
+
+      const int H_out = H - K + 1;
+      const int W_out = W - K + 1;
+      const int MHW = 50 * H_out * W_out;
+      const int HoutWout = H_out * W_out;
+      #define y4d(i3,i2,i1,i0) y[(i3) * MHW + (i2)*HoutWout + (i1)*(W_out) + i0]
+      __shared__ DType tileA[TILE_WIDTH * TILE_HEIGHT];
+      __shared__ DType tileB[TILE_WIDTH * TILE_HEIGHT];
+      #define tA2d(i1, i0) tileA[i1 * TILE_WIDTH + i0]
+      #define tB2d(i1, i0) tileB[i1 * TILE_WIDTH + i0]
+      int b = blockIdx.x; // batch index
+      int bx = blockIdx.y; //
+      int by = blockIdx.z;
+      int tx = threadIdx.x;
+      int ty = threadIdx.y;
+      int Row = by * TILE_WIDTH + ty;
+      int Col = bx * TILE_WIDTH + tx;
+      int numARows = 50, numBRows = 25, numAColumns = K * K, numBColumns = H_out * W_out;
+      DType sum = 0;
+      int aBound = (numAColumns - 1) / TILE_WIDTH + 1; // num of A columns
+      int Aindex, Bindex;
+      for(int m = 0; m < aBound; ++m){
+         Aindex = Row * numAColumns + m * TILE_WIDTH + tx;
+         Bindex = b * 25 * 24 * 24 + (m * TILE_WIDTH + ty) * numBColumns + Col;
+         if(m * TILE_WIDTH + tx < numAColumns && Row < numARows){
+              tA2d(ty, tx) = A[Aindex];
+         }else{
+              tA2d(ty, tx) = 0;
+         }
+         if(m * TILE_WIDTH + ty < numBRows && Col < numBColumns){
+              tB2d(ty, tx) = B[Bindex];
+         }else{
+              tB2d(ty, tx) = 0;
+         }
+         __syncthreads();
+         for (int k = 0; k < TILE_WIDTH; ++k){
+              sum += tA2d(ty, k) * tB2d(k, tx);
+         }
+         __syncthreads();
+      }
+      if(Row < 50 && Col < H_out * W_out){
+            //C[Row * numCColumns + Col] = Cvalue;
+          y4d(b, Row, Col / W_out, Col % W_out) = sum;
+      }
+      #undef y4d
+      #undef tA2d
+      #undef tB2d
+  }
 
 template<typename gpu, typename DType>
-__global__ void forward_kernel(DType *y, const DType *x, const DType *k, const int B, const int M, const int C, const int H, const int W, const int K) {
+__global__ void matrix_kernel(DType *y, DType *B, DType *A) {
+  int row = blockIdx.z * 25 + blockIdx.y;
+  int col = blockIdx.y * 25 + blockIdx.x;
+  int b = blockIdx.x;
 
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
-
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
-    #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
-    #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
-    int X_tile_width = TILE_WIDTH + K - 1;
-    int b, m, local_h, local_w, h_base, w_base, global_h, global_w;
-    extern __shared__ __align__(sizeof(DType)) unsigned char my_smem[];
-    DType *shmem = reinterpret_cast<DType *>(my_smem);
-
-    int W_numOfTiles = (W_out - 1) / TILE_WIDTH + 1;
-    //int H_numOfTiles = (H_out - 1) / TILE_WIDTH + 1;
-    DType* X_shared = &shmem[0];
-    DType* W_shared = &shmem[X_tile_width * X_tile_width];
-    b = blockIdx.x; // batch index 
-    m = blockIdx.y; // output feature map index
-    local_w = threadIdx.x;
-    local_h = threadIdx.y;
-    h_base = (blockIdx.z / W_numOfTiles) * TILE_WIDTH;
-    w_base = (blockIdx.z % W_numOfTiles) * TILE_WIDTH;
-    global_h = h_base + local_h;
-    global_w = w_base + local_w;
-    
-    #define w2d(i1, i0) W_shared[(i1) * K + i0]
-    #define x_shared2d(i1, i0)  X_shared[(i1) * X_tile_width + i0]
-    DType sum = 0;
-    for(int c = 0; c < C; ++c){         // sum over all input channels
-        if(local_h < K && local_w < K){
-            w2d(local_h, local_w) = k4d(m, c, local_h, local_w);
-        }
-        __syncthreads();
-        
-        for(int i = global_h; i < h_base + X_tile_width; i += TILE_WIDTH){
-            for(int j = global_w; j < w_base + X_tile_width; j += TILE_WIDTH){
-                if(i <  H && j < W){
-                    x_shared2d(i - h_base, j - w_base) = x4d(b, c, i, j);
-                }
-            }
-        }
-        __syncthreads();
-        if(global_h < H && global_w < W){
-        for(int p = 0; p < K; ++p){
-            for(int q = 0; q < K; ++q){
-                sum += x_shared2d(local_h + p, local_w + q) * w2d(p, q);
-            }
-        }
-        }
-        __syncthreads();
+  if (row < 50 && col < 24 * 24) {
+    DType val = 0;
+    for (int i = 0; i < 25; i++) {
+      val = val +  A[row * 25 + i] * B[b * 25 * 24 *24 + i * 24 * 24 + col];
     }
-    y4d(b, m, global_h, global_w) = sum;
-    #undef y4d
-    #undef x4d
-    #undef k4d
+    int r = col / 24;
+    int c = col % 24;
+    y[b * 50 * 24 * 24 + row * 24 * 24 + r * 24 + c] = val;
+  }
 }
 
+template<typename gpu, typename DType>
+__global__ void unroll_kernel(DType *k, DType *unroll_kernel,
+  int output_channels, int kernel_y, int kernel_x, int input_channels) {
+  int bid = blockIdx.x;
+  int thx = threadIdx.x;
 
+  //int k_unrolled_size = output_channels * kernel_y * kernel_x * input_channels;
+  int row_size = kernel_y * kernel_x * input_channels;
+  int stride = kernel_y * kernel_x;
+  int kernel_size = kernel_y * kernel_x;
+  // one block per kernel
+  int base = bid * row_size;
+  DType val = k[bid * kernel_size + thx];
 
+  for (int i = thx; i < row_size; i += stride) {
+    unroll_kernel[base + i] = val;
+  }
+}
+
+template<typename gpu, typename DType>
+__global__ void unroll_data(DType *x, DType *x_unrolled, int batch_size,
+  int input_channels, int input_y_size, int input_x_size, int output_y_size,
+  int output_x_size, int kernel_y, int kernel_x) {
+    __shared__ DType channel_for_this_block[28 * 28];
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    // we first compute which input channel we are working on in this batch_size
+    int channel_index = bx * input_channels + by;
+    int width_base = channel_index * kernel_y * kernel_x; // start y pos in chap 16
+    int data_base = channel_index * input_x_size * input_y_size;
+    // load data into shared memory
+    channel_for_this_block[ty * input_x_size + tx] = x[data_base + ty * input_x_size + tx];
+    __syncthreads();
+    // mapping
+    if (tx < output_x_size && ty < output_y_size) {
+      for (int p = 0; p < kernel_y; p++) {
+        for (int q = 0; q < kernel_x; q++) {
+          int w_unroll = width_base + p * kernel_x + q;
+          int h_unroll = ty * output_x_size + tx;
+          x_unrolled[h_unroll * output_y_size * output_x_size + w_unroll] =
+            channel_for_this_block[(ty + p) * input_x_size + tx + q];
+        }
+      }
+    }
+}
 
 // This function is called by new-inl.h
 // Any code you write should be executed by this function
 template<typename gpu, typename DType>
-void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w) {
-    
+void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &k) {
+
 
     // Use mxnet's CHECK_EQ to do assertions.
     // CHECK_EQ(0, 1) << "Starting a GPU implementation based on share memory!";
@@ -91,29 +139,44 @@ void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DT
     // You'll probably need to launch kernels against the right stream to keep MXNet happy
     cudaStream_t s = y.stream_->stream_;
 
-    // Extract the tensor dimensions into B,M,C,H,W,K
-    const int B = x.shape_[0];
-    const int M = w.shape_[0];
-    const int C = x.shape_[1];
-    const int H = x.shape_[2];
-    const int W = x.shape_[3];
-    const int K = w.shape_[2];
+    const int batch_size = x.shape_[0];
+    const int input_channels = x.shape_[1]; // c
+    const int input_y_size = x.shape_[2]; // h
+    const int input_x_size = x.shape_[3]; // w
+    const int output_channels = k.shape_[0]; // kernel number
+    const int kernel_y = k.shape_[2];
+    const int kernel_x = k.shape_[3];
+    const int output_y_size = y.shape_[2];
+    const int output_x_size = y.shape_[3];
 
-    // Set the kernel dimensions
-    // shmem_size = sizeof(DType)*((TILE_WIDTH + K - 1)*(TILE_WIDTH + K - 1) + K * K);
-    int Hout = H - K + 1;
-    int Wout = W - K + 1;
-    int W_numOfTiles = (Wout - 1) / TILE_WIDTH + 1;
-    int H_numOfTiles = (Hout - 1) / TILE_WIDTH + 1;
-    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-    dim3 gridDim(B, M, W_numOfTiles * H_numOfTiles);
+    DType* x_unrolled;
+    DType* k_unrolled;
 
-    // Call the kernel                                0 is sharemem s is stream
-    forward_kernel<gpu, DType><<<gridDim, blockDim, sizeof(DType)*((TILE_WIDTH + K - 1)*(TILE_WIDTH + K - 1) + K * K), s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    // unroll k, each block is incharge of a kernel
+    int k_unrolled_size = output_channels * kernel_y * kernel_x * input_channels;
+    cudaMalloc(&k_unrolled, sizeof(DType) * k_unrolled_size);
+    dim3 k_unroll_block_dim(25, 1, 1);
+    dim3 k_unroll_grid_dim(output_channels, 1, 1);
+    unroll_kernel<gpu, DType><<<k_unroll_grid_dim, k_unroll_block_dim>>>(k.dptr_,
+      k_unrolled, output_channels, kernel_y, kernel_x, input_channels);
 
+    // unroll x
+    int x_unrolled_size = kernel_y * kernel_x * input_channels * output_y_size
+      * output_x_size;
+    cudaMalloc(&x_unrolled, sizeof(DType) * x_unrolled_size);
+    dim3 x_unroll_block_dim(input_x_size, input_y_size, 1);
+    dim3 x_unroll_grid_dim(batch_size, input_channels, 1);
+    unroll_data<gpu, DType><<<x_unroll_grid_dim, x_unroll_block_dim>>>(x.dptr_,
+      x_unrolled, batch_size, input_channels, input_y_size, input_x_size,
+      output_y_size, output_x_size, kernel_y, kernel_x);
+
+      // multi
+      dim3 blockDim(25, 25, 1);
+      dim3 gridDim(batch_size, 24, 2);
+      //forward_kernel<gpu, DType><<<gridDim, blockDim, 2*25*25, s>>>(y.dptr_, x_unrolled, k_unrolled, input_channels, input_y_size, input_x_size, kernel_y);
+    matrix_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_, x_unrolled, k_unrolled);
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-
 }
 
 
