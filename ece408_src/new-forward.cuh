d73980fcd63b1889f094c6f47086e917f759aea0
Yingyi Zhang <yingyiz2@illinois.edu>
2017-11-28 22:54:12 -0600

    clean code of single kernel with width = 16

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -14,33 +14,25 @@ template<typename gpu, typename DType>
 __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridDim B/4, 3, 1,  blockDim 25 * 32
 
     __shared__ DType tileA[1250];    // 50 * 25
-    //__shared__ DType tileB[25 * 32/2 * (6*2)];
     __shared__ DType tileB[4800];
     int b = blockIdx.x;  // batch index
     int bx = blockIdx.y; // 3
     int tx = threadIdx.x; // 32
     int ty = threadIdx.y; // 25
-    //int index = ty * 32 / 2 + tx;
     int index = ty * 16 + tx;
     #define tA2d(i1, i0) tileA[i1 * 50 + i0]
-    //if(tx < 25){
-            //int tileAIndex = (ty - 5) * 32 + tx;
-            //int newTy = tileAIndex / 25;
-            //int newTx = tileAIndex % 25;
-            int kIndex = ty * 25 + tx, tAIndex = tx * 50 + ty; //tAIndex2 = (tx);
-            tileA[tAIndex] = k[kIndex];
-            tileA[tAIndex + TILE_WIDTH] = k[kIndex + 625];
-            if(tx < 9){
-                int tAIndex2 = (tx + 16) * 50 + ty;
-                tileA[tAIndex2] = k[kIndex + 16];
-                tileA[tAIndex2 + TILE_WIDTH] = k[kIndex + 625 + 16];
-            }
-            //tA2d(tx, ty) = k[kIndex];                               //A[Aindex];
-            //tA2d(tx, ty + TILE_WIDTH) = k[kIndex + 625];
-            //tA2d(newTx, newTy) = k[tileAIndex];
-            //tA2d(newTx, newTy + TILE_WIDTH) = k[tileAIndex + 625];
-    //}
-    // load data from global to shared memory
+
+    //----------------------- load filters from global to shared memory ------------------------ 
+    int kIndex = ty * 25 + tx, tAIndex = tx * 50 + ty; //tAIndex2 = (tx);
+    tileA[tAIndex] = k[kIndex];
+    tileA[tAIndex + TILE_WIDTH] = k[kIndex + 625];
+    if(tx < 9){
+        int tAIndex2 = (tx + 16) * 50 + ty;
+        tileA[tAIndex2] = k[kIndex + 16];
+        tileA[tAIndex2 + TILE_WIDTH] = k[kIndex + 625 + 16];
+    }
+    
+    //------------------------- load data from global to shared memory -------------------------
     DType val[12] = {0};
     int tileTmpIndex;
     int local_w, local_h, tileFirstRowIndex, xIndex;
@@ -51,11 +43,8 @@ __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridD
         xIndex = (b + offset) * 784 + local_h * 28 + local_w;
         for(int h = 0; h < 12; ++h){                          // 112
             val[h] = x[xIndex + 56 * h];
-            //val[h] = x[(b + offset) * 784 + 0 * 784 + index + 56 * h];//x4d(b + offset, 0, local_h, local_w);
         }
-        //local_w = index % 28;
-        //local_h = index / 28;
-        for(int i = 0; i < 5; ++i){
+        for(int i = local_h; i >= local_h - 1; --i){
             for(int j = 0; j < 5; ++j){
                 if(local_w - j >= 0 && local_w+5-1-j < 28 && local_h - i >= 0){ //&& local_h + 5 - 1 - i < 28)
                     tileFirstRowIndex = (local_h - i) * 24 + local_w - j;
@@ -77,10 +66,7 @@ __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridD
             xIndex = (b + offset) * 784 + local_h * 28 + local_w;
             for(int h = 0; h < 12; ++h){
                 val[h] = x[xIndex + 56 * h];
-                //val[h] = x[(b + offset) * 784 + 0 * 784 + index + 16 + 56 * h];
             }
-            //local_w = (index + 16) % 28;
-            //local_h = (index + 16) / 28;
         }else{
             local_w = (index - 60) % 12;
             local_h = (index - 60) / 12 + 1;
@@ -89,7 +75,7 @@ __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridD
                 val[h] = x[xIndex + 56 * h];
             }
         }
-        for(int i = 0; i < 5; ++i){
+        for(int i = local_h; i >= local_h - 1; --i){
             for(int j = 0; j < 5; ++j){
                 if(local_w - j >= 0 && local_w+5-1-j < 28 && local_h - i >= 0){// && local_h + 5 - 1 - i<28){
                     tileFirstRowIndex = (local_h - i) * 24 + local_w - j;
@@ -110,11 +96,8 @@ __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridD
         xIndex = (b + offset) * 784 + local_h * 28 + local_w;
         for(int h = 0; h < 12; ++h){
             val[h] = x[xIndex + 56 * h];
-            //val[h] = x[(b + offset) * 784 + 0 * 784 + index + 36 + 56 * h];
         }
-        //local_w = (index + 36) % 28;
-        //local_h = (index + 36) / 28;
-        for(int i = 0; i < 5; ++i){
+        for(int i = local_h; i >= local_h - 1; --i){
             for(int j = 0; j < 5; ++j){
                 if(local_w - j >= 0 && local_w+5-1-j < 28 && local_h - i >= 0){// && local_h + 5 - 1 - i<28){
                     tileFirstRowIndex = (local_h - i) * 24 + local_w - j;
@@ -130,7 +113,8 @@ __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridD
     }
 
     __syncthreads();
-    // compute
+    
+    //----------------------------- compute -------------------------
     DType sum[24] = {0};
     DType tmpA1, tmpA2, tmpB;
     int tileBIndex;
@@ -143,11 +127,11 @@ __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridD
             sum[i * 2 + 1] += tmpA2 * tmpB;
         }
     }
-    //__syncthreads();
 
     //write to global memory
         int col = bx * 16 + tx; // row = ty;
         int tmp = (b + offset) * 28800 + ty *576 + col;
+        /*
         for(int i = 0; i < 12; ++i){
             y[tmp + 48 * i] = sum[i << 1];
         }
@@ -155,12 +139,12 @@ __global__ void matrix_kernel(DType *y, DType *x, DType *k, int offset){ //gridD
         for(int i = 0; i < 12; ++i){
             y[tmp + 48 * i] = sum[i*2 + 1];
         }
-        /*
+        */
         for(int i = 0; i < 24; i += 2){
             y[tmp + 48 * (i >> 1)] = sum[i];//y[tmp + 3 * 32 * (i >> 1)] = sum[i];
             y[tmp + 14400 + 48 * (i >> 1)] = sum[i + 1];//y[tmp + 14400 + 3 * 32 * (i >> 1)] = sum[i + 1];
         }
-        */
+        
     #undef tA2d
 }
 
