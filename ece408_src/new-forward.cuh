7394247eac94ba7f51ac32f54bdbe0463aca5b08
ghe10 <gche1993@gmail.com>
2017-12-13 13:09:50 -0600

    save code for gap block

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -4,7 +4,9 @@
 
 #include <mxnet/base.h>
 
-#define TILE_WIDTH 12
+#define TILE_WIDTH 28
+#define PIC_PER_BLOCK 10
+
 namespace mxnet
 {
 namespace op
@@ -21,52 +23,48 @@ __global__ void forward_kernel(DType *y, const DType *x, const DType *k, const i
     The goal here is to be correct AND fast.
     We have some nice defs for you below to simplify indexing. Feel free to use them, or create your own.
     */
-
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
-    #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
-    int X_tile_width = TILE_WIDTH + K - 1;
-    int b, m, local_h, local_w, h_base, w_base, global_h, global_w;
     extern __shared__ __align__(sizeof(DType)) unsigned char my_smem[];
     DType *shmem = reinterpret_cast<DType *>(my_smem);
+    DType* x_shared = &shmem[0];
 
-    int W_numOfTiles = (W_out - 1) / TILE_WIDTH + 1;
-    DType* X_shared = &shmem[0];
-    b = blockIdx.x; // batch index
-    m = blockIdx.y; // output feature map index
-    local_w = threadIdx.x;
-    local_h = threadIdx.y;
-    h_base = (blockIdx.z / W_numOfTiles) * TILE_WIDTH;
-    w_base = (blockIdx.z % W_numOfTiles) * TILE_WIDTH;
-    global_h = h_base + local_h;
-    global_w = w_base + local_w;
-
-    #define x_shared2d(i1, i0)  X_shared[(i1) * X_tile_width + i0]
-    DType sum = 0;
-    for(int c = 0; c < C; ++c){         // sum over all input channels
-        for(int i = global_h; i < h_base + X_tile_width; i += TILE_WIDTH){
-            for(int j = global_w; j < w_base + X_tile_width; j += TILE_WIDTH){
-                if(i <  H && j < W){
-                    x_shared2d(i - h_base, j - w_base) = x4d(b, c, i, j);
-                }
-            }
-        }
-        __syncthreads();
-        if(global_h < H && global_w < W){
-        for(int p = 0; p < K; ++p){
-            for(int q = 0; q < K; ++q){
-                sum += x_shared2d(local_h + p, local_w + q) *
-                  constant_kernel[m * (C * K * K) + c * (K * K) +  p * (K) + q];
-            }
-        }
-        }
-        __syncthreads();
+    int block_num = blockIdx.x;
+    int local_h = threadIdx.y;
+    int local_w = threadIdx.x;
+    int input_size = 28 * 28;
+    int output_size = 24 * 24;
+    int read_base = block_num * PIC_PER_BLOCK * 28 * 28 + local_h * 28 + local_w;
+    int write_base = 0;
+    DType sum = 0.0;
+    // load 28 * 28 input
+    for (int index = 0; index < PIC_PER_BLOCK; index++) {
+      x_shared[index * input_size + local_h * 28 + local_w] = x[read_base];
+      read_base += B / PIC_PER_BLOCK * input_size; // each loaded data got a jump for 1250 pics
     }
-    y4d(b, m, global_h, global_w) = sum;
-    #undef y4d
-    #undef x4d
-    #undef k4d
+    __syncthreads();
+
+    // compute
+
+    if (local_h < 24 && local_w < 24) {
+      write_base = block_num * 50 * output_size;
+      for (int index = 0; index < PIC_PER_BLOCK; index++) {
+        #pragma unroll 1
+        for (int kernel_index = 0; kernel_index < 50; kernel_index++) {
+          sum = 0.0;
+          for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                sum += x_shared[index * input_size + (local_h + i) * 28 + local_w + j] *
+                  constant_kernel[kernel_index * 25 + i * 5 + j];
+              }
+          }
+
+          // write_base = (block_num) * PIC_PER_BLOCK * 50 * output_size +
+          //   index * (B / PIC_PER_BLOCK) * 50 * output_size
+          //   + kernel_index * output_size + local_h * 24 + local_w;
+          y[write_base + kernel_index * output_size + local_h * 24 + local_w] = sum; // this should be kernel related
+        }
+        write_base += B / PIC_PER_BLOCK * 50 * output_size;
+      }
+    }
 }
 
 
@@ -85,25 +83,25 @@ void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DT
     cudaStream_t s = y.stream_->stream_;
 
     // Extract the tensor dimensions into B,M,C,H,W,K
-    const int B = x.shape_[0];
-    const int M = w.shape_[0];
-    const int C = x.shape_[1];
-    const int H = x.shape_[2];
-    const int W = x.shape_[3];
-    const int K = w.shape_[2];
+    const int B = x.shape_[0]; // input batch
+    const int M = w.shape_[0]; // output channel number
+    const int C = x.shape_[1]; // input channel number
+    const int H = x.shape_[2]; // input height
+    const int W = x.shape_[3]; // input width
+    const int K = w.shape_[2]; // kernel size
 
     // Set the kernel dimensions
-    int Hout = H - K + 1;
-    int Wout = W - K + 1;
-    int W_numOfTiles = (Wout - 1) / TILE_WIDTH + 1;
-    int H_numOfTiles = (Hout - 1) / TILE_WIDTH + 1;
+    // int Hout = H - K + 1;
+    // int Wout = W - K + 1;
+    // int W_numOfTiles = (Wout - 1) / TILE_WIDTH + 1;
+    // int H_numOfTiles = (Hout - 1) / TILE_WIDTH + 1;
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-    dim3 gridDim(B, M, W_numOfTiles * H_numOfTiles);
+    dim3 gridDim((B / PIC_PER_BLOCK), 1, 1);
 
     // allocate constant_kernel
     cudaMemcpyToSymbol(constant_kernel, w.dptr_, sizeof(float) * 50 * 25, 0, cudaMemcpyDeviceToDevice);
     // Call the kernel                                0 is sharemem s is stream
-    forward_kernel<gpu, DType><<<gridDim, blockDim, sizeof(DType)*((TILE_WIDTH + K - 1)*(TILE_WIDTH + K - 1) + K * K), s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<gpu, DType><<<gridDim, blockDim, PIC_PER_BLOCK * sizeof(DType) * H * W, s>>>(y.dptr_,x.dptr_,w.dptr_, B, M, C, H, W, K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
