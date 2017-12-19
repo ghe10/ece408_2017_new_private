118a1b8f4b30d843e249d02d762a9b43f943790f
Yingyi Zhang <yingyiz2@illinois.edu>
2017-12-19 00:11:42 -0600

    add check condition for any number of B

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -11,7 +11,7 @@ namespace op
 
 __constant__ float kernel[1250];
 
-__global__ void forward_kernel(float *y, const float *x, const float *k) {
+__global__ void forward_kernel(float *y, const float *x, const float *k, int xBound, int yBound) {
     /*
       A few constants are used in this kernel to reduce computation
     */
@@ -26,9 +26,12 @@ __global__ void forward_kernel(float *y, const float *x, const float *k) {
     float sum;
     // load 28 * 28 input
     for (int index = 0; index < 17; index++) {
-      x_shared[(index<<6)*9 + local_index] = x[read_base + (index<<6)*9];
+      x_pos = read_base + (index<<6)*9;
+      if(x_pos < xBound){
+        x_shared[(index<<6)*9 + local_index] = x[x_pos];
+      }
     }
-    if(local_index < 400){
+    if(local_index < 400 && read_base + 9792 < xBound){
         x_shared[9792 + local_index] = x[read_base + 9792];
     }
     __syncthreads();
@@ -37,6 +40,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k) {
       for (int index = 0; index < 13; index++) {
         x_pos = (index<<4)*49 + (((local_index >> 3)/3) << 2) * 7 + local_index%24;
         y_pos = write_base + (index << 7)*225;
+        if(y_pos < yBound){
         #pragma unroll 15
         for (int kernel_index = 0; kernel_index < 50; kernel_index++) {
           sum = 0;
@@ -48,6 +52,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k) {
           }
           y[y_pos + (kernel_index << 6) * 9] = sum;
         }
+        }
       }
     __syncthreads();
 
@@ -55,15 +60,19 @@ __global__ void forward_kernel(float *y, const float *x, const float *k) {
     read_base += 10192;   //7840;
     write_base += 374400; //288000;
     for (int index = 0; index < 16; index++) {
-      x_shared[(index<<6)*9 + local_index] = x[read_base + (index<<6)*9];
+      x_pos = read_base + (index<<6)*9;
+      if(x_pos < xBound){
+        x_shared[(index<<6)*9 + local_index] = x[x_pos];
+      }
     }
-    if(local_index < 192) x_shared[9216 + local_index] = x[read_base + 9216];
+    if(local_index < 192 && read_base + 9214 < xBound) x_shared[9216 + local_index] = x[read_base + 9216];
     __syncthreads();
 
     // compute the convolution result
       for (int index = 0; index < 12; index++) {
         x_pos = (index<<4)*49 + (((local_index>>3)/3) << 2) * 7 + local_index%24;
         y_pos = write_base + (index << 7)*225;
+        if(y_pos < yBound){
         #pragma unroll 15
         for (int kernel_index = 0; kernel_index < 50; kernel_index++) {
           sum = 0;
@@ -75,6 +84,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k) {
           }
           y[y_pos + (kernel_index<<6) * 9] = sum;
         }
+        }
       }
 }
 
@@ -85,15 +95,15 @@ __global__ void forward_kernel(float *y, const float *x, const float *k) {
 // Any code you write should be executed by this function
 template<>
 void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w) {
-    //const int B = x.shape_[0] / 25; // input batch
+    const int B = x.shape_[0]; // input batch
 
     dim3 blockDim(64, 9, 1);
-    dim3 gridDim(x.shape_[0] / 25, 1, 1);
+    dim3 gridDim((B - 1) / 25 + 1, 1, 1);
 
     // allocate constant_kernel
     cudaMemcpyToSymbol(kernel, w.dptr_, 5000, 0, cudaMemcpyDeviceToDevice);
     // Call the kernel                                0 is sharemem s is stream
-    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_);
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, (B<<4)*49, (B<<6)*9);
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
